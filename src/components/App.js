9b9ce5be9a651c667ed5cf7479c7bd849f588351
Josh Kim <9492648646m@gmail.com>
2023-01-02 16:29:23 -0600

    429pm

--- src/components/App.js
+++ src/components/App.js
@@ -1,82 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { useBootstrapBreakpoints } from 'react-bootstrap/esm/ThemeProvider';
+// import './Styles.css'
+import 'semantic-ui-css/semantic.min.css'
+import { Route, Switch, BrowserRouter } from "react-router-dom";// import Header from "./Header";
+
+import React, {useState, useEffect, Component} from "react";
+import { Button, Container, Grid, Header, Icon, Menu } from "semantic-ui-react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+// import NavBar from "./NavBar"
 
 function App () {
-    const [book, setBook] = useState('');
-    const [chapter, setChapter] = useState('');
-    const [verse, setVerse] = useState('');
-    const [verseText, setVerseText] = useState('');
-    const [books, setBooks] = useState([])
-  
-      useEffect(() => {
-        // make a GET request to the Bible API endpoint
-        fetch(`https://bible-api.com/${book}%20${chapter}:${verse}`)
-          .then(response => response.json())
-          .then(data => {
-            setVerseText(data.text);
-            setBooks(data);
-          });
-      }, [book, chapter, verse]);
-  
-    console.log("books", book)
-  
-    
-   
-  
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-  
-      // make a GET request to the Bible API endpoint
-      fetch(`https://bible-api.com/${book}%20${chapter}:${verse}`)
-        .then(response => response.json())
-        .then(data => {
-          setVerseText(data.text);
-  
-          // make a POST request to the /read endpoint
-          fetch('http://localhost:6001/read', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              book: book,
-              chapter: chapter,
-              verse: verse,
-              text: data.text
-            })
-          });
-        });
-    }
-  
     return (
-      <div>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Book:
-            <input type="text" value={book} onChange={(event) => setBook(event.target.value)} />
-          </label>
-          <br />
-          <label>
-            Chapter:
-            <input type="number" value={chapter} onChange={(event) => setChapter(event.target.value)} />
-          </label>
-          <br />
-          <label>
-            Verse:
-            <input type="number" value={verse} onChange={(event) => setVerse(event.target.value)} />
-          </label>
-          <br />
-          <button type="submit">Submit</button>
-        </form>
-        {verseText && <p>{verseText}</p>}
-        {/* <ul>
-        {books.map((book) => {
-          <li key = {book.id}>{book.name}</li>
-        })}
-        </ul> */}
-      </div>
-    );
-  }
+    <BrowserRouter> 
+    <div>
+        <h1> NavBar</h1>
+        <NavBar />
+    </div>
+    <div>
+        <Switch>
+            <Route path = "/">
+                <Home />
+            </Route>
+        </Switch>
+    </div>
+    </BrowserRouter>
+    )
+}
 
 export default App;
