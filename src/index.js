d6d470dd8200981cb6838082fa1ed8d35236640b
Josh Kim <9492648646m@gmail.com>
2023-01-02 22:02:57 -0600

    0102-2023

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
+//bibleproject-01-02-2023-awsamplify GITHUB//
 
 
 ReactDOM.render(
