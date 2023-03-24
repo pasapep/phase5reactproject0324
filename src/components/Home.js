c89df43ebbb0db767749fce5577222c0df5908e9
josh <9492648646m@gmail.com>
2023-03-24 00:33:13 -0500

    saveprogress

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,115 +1,95 @@
-import React, {useState, useEffect, Component} from "react";
-import { Menu, Container , Header, Divider, Grid, Button } from "semantic-ui-react";
-import Progress from "./Progress"
+import React, {useState, useEffect} from "react";
+import { Menu, Container, Header, Divider, Grid, Button } from "semantic-ui-react";
+import Progress from "./Progress";
 
-function Home () { 
-    const [book, setBook] = useState('');
-    const [chapter, setChapter] = useState('');
-    const [verse, setVerse] = useState('');
-    const [verseText, setVerseText] = useState('');
-    const [newBooks, setNewBooks] = useState([]);
+function Home() { 
+  const [book, setBook] = useState('');
+  const [chapter, setChapter] = useState('');
+  const [verse, setVerse] = useState('');
+  const [verseText, setVerseText] = useState('');
+  const [newBooks, setNewBooks] = useState([]);
+  const [bookLists, setBookLists] = useState([]);
 
+  useEffect(() => {
+    // fetch the list of books from the Rails backend
+    fetch("http://localhost:3000/books")
+      .then(response => response.json())
+      .then(data => {
+        setBookLists(data);
+      });
+  }, []);
 
-    useEffect(() => {
-        // make a GET request to the Bible API endpoint
-        fetch(`https://bible-api.com/${book}%20${chapter}:${verse}`)
-          .then(response => response.json())
-          .then(data => {
-            setVerseText(data.text);
-            setNewBooks(data);
-          });
-      }, [book, chapter, verse]);
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    //Using useEffect to fetch the books. 
-    const [bookLists, setBookLists] = useState([]);
-    const [verseLists, setVerseLists] = useState([]);
-    useEffect(() => {
-        fetch ("http://localhost:5555/listerines")
-        .then (resp => resp.json())
-        .then (data => {
-            setBookLists(data);
-            setVerseLists(data);
-        })
-    }, [])
-
-
-    const blueBook = bookLists.map((book) => book.book)
-    console.log("bluebook",blueBook)
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        fetch (`https://bible-api.com/${book}%20${chapter}:${verse}`)
-            .then (response => response.json())
-            .then (data => {
-                setVerseText(data.text);
-           
+    // fetch the text of the selected verse from the Bible API
+    fetch(`https://bible-api.com/${book}%20${chapter}:${verse}`)
+      .then(response => response.json())
+      .then(data => {
+        setVerseText(data.text);
         
-            fetch('http://localhost:5555/read', {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    book: book,
-                    chapter: chapter,
-                    verse: verse,
-                    text: data.text
-                })
-            })
-            })
-    }
+        // save the reading progress to the Rails backend
+        fetch("http://localhost:3000/reading_progress", {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            book: book,
+            chapter: chapter,
+            verse: verse,
+            text: data.text
+          })
+        });
+      });
+  };
 
+  const blueBook = bookLists.map((book) => book.name);
 
-
-    return (
-        <Container text textAlign="center">
-        <Divider hidden />  
-        <Header size="huge">Bible App</Header>
-        <div className = "App">
-            <form onSubmit = {handleSubmit}>
-                <Header size = "huge" as="h1"> {verseText && <p>{verseText}</p>}</Header>
-            <Grid stackable container>
+  return (
+    <Container text textAlign="center">
+      <Divider hidden />  
+      <Header size="huge">Bible App</Header>
+      <div className="App">
+        <form onSubmit={handleSubmit}>
+          <Header size="huge" as="h1">
+            {verseText && <p>{verseText}</p>}
+          </Header>
+          <Grid stackable container>
             <Grid.Row columns="three">
-            <Grid.Column>
-              <Header size="huge" as="h1">
-                Book
-              </Header>
-              {/* Change with the list */}
-              <input list = "browsers" type = "text" value = {book} onChange = {(event) => setBook(event.target.value) } />
-              <datalist id = "browsers" value = {book} onChange = {(event) => setBook(event.target.value)}>
-                {blueBook.map((book) => (
-                  <option key = {book} value = {book}> {book} </option>
-                ))}
-              </datalist>
-            </Grid.Column>
-            <Grid.Column>
-              <Header size="huge" as="h1">
-                Chapter
-              </Header>
-              <input type = "number" value = {chapter} onChange = {(event) => setChapter(event.target.value)} />
-              
-            </Grid.Column>
-            <Grid.Column>
-              <Header size="huge" as="h1">
-                Verse
-              </Header>
-              <input type = "number" value = {verse} onChange = {(event) => setVerse(event.target.value)} />
-            </Grid.Column>
-            
-        
-          </Grid.Row>
-        </Grid>
-            <Divider hidden />
-            <button type = "submit"> Send to DB JSON </button>
-
-            </form>
+              <Grid.Column>
+                <Header size="huge" as="h1">
+                  Book
+                </Header>
+                {/* Use the list of books fetched from the Rails backend */}
+                <input list="browsers" type="text" value={book} onChange={(event) => setBook(event.target.value)} />
+                <datalist id="browsers">
+                  {blueBook.map((book) => (
+                    <option key={book} value={book}>{book}</option>
+                  ))}
+                </datalist>
+              </Grid.Column>
+              <Grid.Column>
+                <Header size="huge" as="h1">
+                  Chapter
+                </Header>
+                <input type="number" value={chapter} onChange={(event) => setChapter(event.target.value)} />
+              </Grid.Column>
+              <Grid.Column>
+                <Header size="huge" as="h1">
+                  Verse
+                </Header>
+                <input type="number" value={verse} onChange={(event) => setVerse(event.target.value)} />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+          <Divider hidden />
+          <Button type="submit" primary>Save Progress</Button>
+        </form>
         <Divider hidden />
-        <Progress     bookLists = {bookLists} verseLists = {verseLists} setBookLists = {setBookLists} /> 
-        </div>
-        </Container>
-        
-    )
+      </div>
+    </Container>
+  );
 }
 
 export default Home;
--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,42 +1,186 @@
-import React, {useState, useEffect, Component} from "react";
-import { Menu, Container, List, Icon , Divider } from "semantic-ui-react";
+// import React, { useState, useEffect } from "react";
 
-function Progress ({bookLists}) { 
-    const [isDarkMode, setIsDarkMode] = useState(true);
-    const onToggleMode = () => {
-        setIsDarkMode((isDarkMode) => !isDarkMode)
+// const Progress = () => {
+//   const [books, setBooks] = useState([
+//     { name: "Genesis", chapters: 50 },
+//     { name: "Exodus", chapters: 40 },
+//     { name: "Leviticus", chapters: 27 },
+//     { name: "Numbers", chapters: 36 },
+//     { name: "Deuteronomy", chapters: 34 },
+//     { name: "Joshua", chapters: 24 },
+//     { name: "Judges", chapters: 21 },
+//     { name: "Ruth", chapters: 4 },
+//     { name: "1 Samuel", chapters: 31 },
+//     { name: "2 Samuel", chapters: 24 },
+//     { name: "1 Kings", chapters: 22 },
+//     { name: "2 Kings", chapters: 25 },
+//     { name: "1 Chronicles", chapters: 29 },
+//     { name: "2 Chronicles", chapters: 36 },
+//     { name: "Ezra", chapters: 10 },
+//     { name: "Nehemiah", chapters: 13 },
+//     { name: "Esther", chapters: 10 },
+//     { name: "Job", chapters: 42 },
+//     { name: "Psalms", chapters: 150 },
+//     { name: "Proverbs", chapters: 31 },
+//     { name: "Ecclesiastes", chapters: 12 },
+//     { name: "Song of Solomon", chapters: 8 },
+//     { name: "Isaiah", chapters: 66 },
+//     { name: "Jeremiah", chapters: 52 },
+//     { name: "Lamentations", chapters: 5 },
+//     { name: "Ezekiel", chapters: 48 },
+//     { name: "Daniel", chapters: 12 },
+//     { name: "Hosea", chapters: 14 },
+//     { name: "Joel", chapters: 3 },
+//     { name: "Amos", chapters: 9 },
+//     { name: "Obadiah", chapters: 1 },
+//     { name: "Jonah", chapters: 4 },
+//     { name: "Micah", chapters: 7 },
+//     { name: "Nahum", chapters: 3 },
+//     { name: "Habakkuk", chapters: 3 },
+//     { name: "Zephaniah", chapters: 3 },
+//     { name: "Haggai", chapters: 2 },
+//     { name: "Zechariah", chapters: 14 },
+//     { name: "Malachi", chapters: 4 },
+//     { name: "Matthew", chapters: 28 },
+//     { name: "Mark", chapters: 16 },
+//     { name: "Luke", chapters: 24 },
+//     { name: "John", chapters: 21 },
+//     { name: "Acts", chapters: 28 },
+//     { name: "Romans", chapters: 16 },
+//     { name: "1 Corinthians", chapters: 16 },
+//     { name: "2 Corinthians", chapters: 13 },
+//     { name: "Galatians", chapters: 6 },
+//     { name: "Ephesians", chapters: 6 },
+//     { name: "Philippians", chapters: 4 },
+//     { name: "Colossians", chapters: 4 },
+//     { name: "1 Thessalonians", chapters: 5 },
+//     { name: "2 Thessalonians", chapters: 3 },
+//     { name: "1 Timothy", chapters:6},
+//     { name: "2 Timothy", chapters: 4 },
+//     { name: "Titus", chapters: 3 },
+//     { name: "Philemon", chapters: 1 },
+//     { name: "Hebrews", chapters: 13 },
+//     { name: "James", chapters: 5 },
+//     { name: "1 Peter", chapters: 5 },
+//     { name: "2 Peter", chapters: 3 },
+//     { name: "1 John", chapters: 5 },
+//     { name: "2 John", chapters: 1 },
+//     { name: "3 John", chapters: 1 },
+//     { name: "Jude", chapters: 1 },
+//     { name: "Revelation", chapters: 22 }
+//     ]);
+
+//     useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await fetch("https://bible-api.com/books");
+//         const data = await response.json();
+//         setBooks(
+//           data.map((book) => ({
+//             name: book.name,
+//             chapters: book.chapters.length,
+//           }))
+//         );
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+//     fetchData();
+//   }, []);
+
+//   const displayChapters = (count) => {
+//     let chapterArr = [];
+//     for (let i = 1; i <= count; i++) {
+//       chapterArr.push(i);
+//     }
+//     let chapterGroups = [];
+//     while (chapterArr.length > 0) {
+//       chapterGroups.push(chapterArr.splice(0, 10));
+//     }
+//     return chapterGroups.map((group, index) => (
+//       <div className="chapter-group" key={index}>
+//         {group.map((chapter) => (
+//           <div className="chapter-circle" key={chapter}>
+//             {chapter}
+//           </div>
+//         ))}
+//       </div>
+//     ));
+//   };
+
+//   return (
+//     <div>
+//       <h2>Progress</h2>
+//       <ul>
+//         {books.length > 0 ? (
+//           books.map((book) => (
+//             <li key={book.name}>
+//               <div className="book-title">{book.name}</div>
+//               <div className="chapter-count">{displayChapters(book.chapters)}</div>
+//             </li>
+//           ))
+//         ) : (
+//           <p>Loading...</p>
+//         )}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default Progress;
+
+import React, { useState, useEffect } from "react";
+
+const Progress = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://bible-api.com/books");
+        const data = await response.json();
+        setBooks(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const displayChapters = (count) => {
+    const chapterGroups = [];
+    for (let i = 1; i <= count; i += 10) {
+      chapterGroups.push(
+        Array.from({ length: 10 }, (_, index) => i + index).filter(
+          (chapter) => chapter <= count
+        )
+      );
     }
-    const [color, setColor] = useState(true);
+    return chapterGroups.map((group, index) => (
+      <div className="chapter-group" key={index}>
+        {group.map((chapter) => (
+          <div className="chapter-circle" key={chapter}>
+            {chapter}
+          </div>
+        ))}
+      </div>
+    ));
+  };
 
-    const toggleColor = () => {
-        setColor(color ==='black' ? 'black' : 'magenta')
-    }
+  return (
+    <div>
+      <h2>Progress</h2>
+      <ul>
+        {books.map((book) => (
+          <li key={book.name}>
+            <div className="book-title">{book.name}</div>
+            <div className="chapters">{displayChapters(book.chapters)}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-    const textToggle = isDarkMode ? "Read" : "Unread"
-    const newBooks = bookLists.map(book => book.book);
-    const newChapters = bookLists.map(book => book.chapter)
-    const sellers = bookLists.map(book => Array.from({length:book.chapter}, (_,i) => (
-        <li onClick = {toggleColor} style = {{color} } key = {i} >{textToggle}{i + 1} </li>
-    )))
-
-    return ( 
-        <div>
-        <List.Item>
-            <Divider hidden />
-            <List.Icon name = 'github' size = 'large'  />
-            <Divider hidden />
-            <List.Content>
-            {bookLists.map(book =>
-                 <List>{book.book}</List>
-                 )}
-            {/* {Array.from({length: newChapters.chapter}, (_,i) => (
-                    <button style = {isDarkMode? {color:"black"}:{color:"magenta"}} key = {i}>{i +1} </button>
-                ))} */}
-
-              
-            </List.Content>
-        </List.Item>        </div>
-    )
-}
 
 export default Progress;
